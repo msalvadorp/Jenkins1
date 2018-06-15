e73b71ef1af003b2ff5b7e1312c9ba2235d7687e
Miguel Salvador <msalvadorp@hotmail.com>
2018-06-15 17:45:01 -0500

    Subiendo mi proyecto mvc

---
+++ PruebasUnitarias/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int valor1 = 10;
+            int valor2 = 20;
+
+            int resp = 30;
+
+            Assert.IsTrue(resp == 30, "Salio mal");
+
+        }
+    }
+}
